6d5a72e387435f1ecba85a81ff383012ab4e40d1
JiaDingYi <wzy2010416033@163.com>
2021-05-27 14:45:25 +0800

    Initial Commit

---
+++ AASDKDemo-iOS/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AASDKDemo-iOS
+//
+//  Created by jdy on 2021/5/27.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+

37039a7367a876b0aaadcba6b82693a60c379111
JiaDingYi <wzy2010416033@163.com>
2021-06-10 11:58:53 +0800

    add new controllers

---
+++ AASDKDemo-iOS/AwardAlertForZombieController.h
@@ -0,0 +1,16 @@
+//
+//  AwardAlertForZombieController.h
+//  AASDKDemo-iOS
+//
+//  Created by jdy on 2021/6/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AwardAlertForZombieController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AASDKDemo-iOS/RewardedInfoForZombieController.m
@@ -0,0 +1,31 @@
+//
+//  RewardedInfoForZombieController.m
+//  AASDKDemo-iOS
+//
+//  Created by jdy on 2021/6/10.
+//
+
+#import "RewardedInfoForZombieController.h"
+
+@interface RewardedInfoForZombieController ()
+
+@end
+
+@implementation RewardedInfoForZombieController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

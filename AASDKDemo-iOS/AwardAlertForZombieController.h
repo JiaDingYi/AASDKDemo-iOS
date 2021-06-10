c6a367a84a555c413c3032a144be3cdc070bc255
JiaDingYi <wzy2010416033@163.com>
2021-06-10 14:34:00 +0800

    build award alert controller ui

--- AASDKDemo-iOS/AwardAlertForZombieController.h
+++ AASDKDemo-iOS/AwardAlertForZombieController.h
@@ -8,8 +8,14 @@
 #import <UIKit/UIKit.h>
 
 NS_ASSUME_NONNULL_BEGIN
+@protocol AwardAlertInfoControllerDelegate <NSObject>
+- (void)userClickLeaveButton;
+- (void)userClickAuthButton;
+
+@end
 
 @interface AwardAlertForZombieController : UIViewController
+@property (nonatomic, weak) id<AwardAlertInfoControllerDelegate> delegate;
 
 @end
 
--- AASDKDemo-iOS/AwardAlertForZombieController.m
+++ AASDKDemo-iOS/AwardAlertForZombieController.m
@@ -6,6 +6,7 @@
 //
 
 #import "AwardAlertForZombieController.h"
+#import <Masonry/Masonry.h>
 
 @interface AwardAlertForZombieController ()
 
@@ -28,7 +29,56 @@
 }
 
 - (void)setUpUI {
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor clearColor];
+    UIImageView *bgImg = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"zombie_leave_bg"]];
+    bgImg.userInteractionEnabled = YES;
+    [self.view addSubview:bgImg];
+    CGFloat bgWidth = self.view.frame.size.width * 0.6;
+    CGFloat bgHeight = bgWidth / 1.42;
+    [bgImg mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(self.view);
+        make.centerY.equalTo(self.view);
+        make.width.mas_equalTo(bgWidth);
+        make.height.mas_equalTo(bgHeight);
+    }];
+    
+    UIButton *leaveButton = [[UIButton alloc] init];
+    [leaveButton setBackgroundImage:[UIImage imageNamed:@"zombie_leave_button"] forState:UIControlStateNormal];
+    [leaveButton addTarget:self action:@selector(userClickLeaveButton) forControlEvents:UIControlEventTouchUpInside];
+    [bgImg addSubview:leaveButton];
+    CGFloat buttonWidth = bgWidth * 0.33;
+    CGFloat buttonheight = buttonWidth / 3.0;
+    CGFloat bottomMargin = bgHeight * 0.085;
+    [leaveButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(bgImg.mas_centerX).multipliedBy(0.6);
+        make.bottom.equalTo(bgImg.mas_bottom).offset(-bottomMargin);
+        make.width.mas_equalTo(buttonWidth);
+        make.height.mas_equalTo(buttonheight);
+    }];
+    
+    UIButton *authButton = [[UIButton alloc] init];
+    [authButton setBackgroundImage:[UIImage imageNamed:@"zombie_auth_button"] forState:UIControlStateNormal];
+    [authButton addTarget:self action:@selector(userClickAuthButton) forControlEvents:UIControlEventTouchUpInside];
+    [bgImg addSubview:authButton];
+    [authButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(bgImg.mas_centerX).multipliedBy(1.4);
+        make.bottom.equalTo(leaveButton.mas_bottom);
+        make.width.height.equalTo(leaveButton);
+    }];
+}
+
+- (void)userClickLeaveButton {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(userClickLeaveButton)]) {
+        [self.delegate userClickAuthButton];
+        [self dismissViewControllerAnimated:NO completion:nil];
+    }
+}
+
+- (void)userClickAuthButton {
+    if (self.delegate && [self.delegate respondsToSelector:@selector(userClickAuthButton)]) {
+        [self.delegate userClickAuthButton];
+        [self dismissViewControllerAnimated:NO completion:nil];
+    }
 }
 
 @end

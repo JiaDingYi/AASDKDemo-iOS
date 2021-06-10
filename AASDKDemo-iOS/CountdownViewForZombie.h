b542658b6ee809a6cf8731ac6b9a88fef3a0166b
JiaDingYi <wzy2010416033@163.com>
2021-06-10 14:53:01 +0800

    add countdown view

---
+++ AASDKDemo-iOS/CountdownViewForZombie.h
@@ -0,0 +1,16 @@
+//
+//  CountdownViewForZombie.h
+//  AASDKDemo-iOS
+//
+//  Created by jdy on 2021/6/10.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CountdownViewForZombie : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AASDKDemo-iOS/CountdownViewForZombie.m
@@ -0,0 +1,13 @@
+//
+//  CountdownViewForZombie.m
+//  AASDKDemo-iOS
+//
+//  Created by jdy on 2021/6/10.
+//
+
+#import "CountdownViewForZombie.h"
+
+@implementation CountdownViewForZombie
+
+
+@end
